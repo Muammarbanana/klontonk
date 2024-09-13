8a44616b91041126a8f0346fac8bcd870c154d11
Muammarbanana <muammarfanani2@gmail.com>
2024-09-13 23:26:32 +0800

    chore: change icon launcher and splash

--- lib/features/products/presentation/view/create_product_page.dart
+++ lib/features/products/presentation/view/create_product_page.dart
@@ -119,7 +119,7 @@ class _CreateProductPageState extends State<CreateProductPage> {
                       );
                     },
                   ),
-                )
+                ),
               ],
             ),
           ),
